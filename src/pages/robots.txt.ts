909e9450abd09cb321dee2da5649d9801d52223f
TanKimzeg <1964375900@qq.com>
2025-10-29 22:38:05 +0800

    1029 update

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,6 +2,24 @@ import type { APIRoute } from "astro";
 
 function getRobotsTxt(sitemapURL: URL) {
   return `
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: MJ12bot
+Disallow: /
+
+User-agent: SemrushBot
+Disallow: /
+
+User-agent: DotBot
+Disallow: /
+
+User-Agent: AhrefsBot
+Disallow: /
+
+User-Agent: MauiBot
+Disallow: /
+
 User-agent: *
 Allow: /
 Sitemap: ${sitemapURL.href}
